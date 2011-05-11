583c91adbbaef80de748d5a6781d009f9826fc4c
Mike Bethany <mikbe.tk@gmail.com>
2011-05-11 17:08:26 -0400

    fixed commandable app_exe name, was not set to widget

--- lib/widget/commandable_config.rb
+++ lib/widget/commandable_config.rb
@@ -2,7 +2,7 @@
 require 'commandable'
 Commandable.color_output = true
 Commandable.verbose_parameters = false
-Commandable.app_exe = "commandable"
+Commandable.app_exe = "widget"
 Commandable.app_info =
 """
   \e[92mWidget!\e[0m - It makes stuff and things! Exclamation!
